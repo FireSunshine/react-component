c4153a0a0f1d5058dbdb538ad15faa39ed5d9a6f
sunshine <2898205320@qq.com>
2024-03-06 10:57:40 +0800

    配置 eslint 和 prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,11 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended', // 使用 Prettier 推荐的规则
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +15,11 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off', 
+    'prettier/prettier': 'error',
+    'no-undef': 'error', 
+    'no-console': 'warn', 
+    eqeqeq: 'error', 
   },
-}
+};
